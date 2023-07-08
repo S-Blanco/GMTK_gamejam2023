bddf0c790563e42dc82fe4a7e3aa00bbfb73b0af
Yuglut <martin.farnir@uliege.be>
2023-07-08 18:57:40 +0100

    Stop movement

--- Game/Src/TestEnemyMovement.gd
+++ Game/Src/TestEnemyMovement.gd
@@ -6,27 +6,40 @@ extends Control
 # var b = "text"
 #onready var Enemy = $Enemy
 onready var Bckgnd = $Background/TextureRect
-onready var stopScroll = 500
+onready var charPlayer = $Character/AnimationPlayer
+onready var charSprite = $Character/Sprite
+onready var stopScroll = 800
 onready var scrollDist = 0
+onready var isSlowing = false
+
 export(PackedScene) var Enemy_scn = preload("res://Src/Characters/Enemy.tscn")
+export(int) var startSlow = 1200
 export(int) var spawn_position = 2800
 export(int) var spawn_y = 820
+export(float) var slowTime = 1.5
 
 var base_scroll_speed
 var base_pixel_speed
 var curr_pixel_speed
+var curr_scroll_speed
+var init_child_num
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	base_scroll_speed = Bckgnd.scroll_speed
 	base_pixel_speed = base_scroll_speed*Bckgnd.texture.get_size().x
 	curr_pixel_speed = base_pixel_speed
+	curr_scroll_speed = base_scroll_speed
+	init_child_num = len(self.get_children())
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var New_enemy
+	var children
+	var closest_enemy
+	var NMX
 	scrollDist += delta*base_pixel_speed
 #	print(scrollDist,' ',fmod(scrollDist,spawn_position),' ',delta*curr_pixel_speed,' ',spawn_position-delta*curr_pixel_speed)
 	if (spawn_position-delta*curr_pixel_speed<fmod(scrollDist,spawn_position)) || (fmod(scrollDist,spawn_position)<delta*curr_pixel_speed):
@@ -35,5 +48,38 @@ func _process(delta):
 		New_enemy.set_global_position(Vector2(spawn_position,spawn_y))
 		New_enemy.pixel_speed=curr_pixel_speed
 		add_child(New_enemy)
-	for _i in self.get_children():
-		print(_i)
+	children = self.get_children()
+	if len(children)>init_child_num:
+		closest_enemy = children[init_child_num]
+		NMX = closest_enemy.get_global_position().x
+		if (NMX<stopScroll+delta*curr_pixel_speed) && (NMX>stopScroll-delta*curr_pixel_speed):
+			woah_there(children,Bckgnd)
+
+#func slow_down(closest_enemy,startX,endX,slowTime):
+#	var tween = get_node("Tween")
+#	curr_pixel_speed = 0.0
+#	tween.interpolate_property(closest_enemy, "position:x",
+#		   startX,endX,slowTime,
+#		   Tween.TRANS_CUBIC, Tween.EASE_IN)
+#	tween.interpolate_property(Bckgnd, "material:shader_param/scroll_speed",
+#		   base_pixel_speed,0.0,slowTime,
+#		   Tween.TRANS_CUBIC, Tween.EASE_IN)
+#	tween.start()
+#	isSlowing = true
+#	pass
+#
+#func speed_up():
+#	isSlowing = false
+#	var tween = get_node("Tween")
+#	curr_pixel_speed = base_pixel_speed
+##	tween.interpolate_property(closest_enemy, "position:x",
+##	       startX,endX,slowTime,
+##		   Tween.TRANS_CUBIC, Tween.EASE_IN)
+##	tween.start()
+
+func woah_there(children,bckgnd):
+	bckgnd.set_scroll_speed(0.0)
+	for i in range(init_child_num,len(children)):
+		children[i].pixel_speed = 0.0
+	charPlayer.stop()
+	pass
