8251e53dfe134b96e7fb5abab98c39601f7d7f59
SylvainBlanco <s.blanco@ulg.ac.be>
2023-07-09 11:54:57 +0200

    move enemy vars out of loop

--- Game/Src/TestEnemyMovement.gd
+++ Game/Src/TestEnemyMovement.gd
@@ -1,9 +1,5 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 #onready var Enemy = $Enemy
 onready var Bckgnd = $Background/TextureRect
 onready var charPlayer = $Character/AnimationPlayer
@@ -27,15 +23,21 @@ var init_child_num
 # For the power ups, 0 means empty
 # number from 1 to 9 will be associated
 # with a unique power up/down
-var power_slots = [0,0]
+# the list of powers should be stored in an enum
+# later so we can write, e.g. power_slots[0]=fire
+enum powers {no_power,
+			lightning,
+			fire}
+var power_slots = [powers.no_power,powers.no_power]
 
 onready var player_controls = $PlayerControls
 onready var power1_toggle = $Power1Toggle
 onready var power2_toggle = $Power2Toggle
 
-
-# onready var testt = get_node("res://Src/UI/PlayerControls.tscn")
-# testt.connect("power1",self,_on_power1)
+var New_enemy # why do we need to create a new var enemy each frame?
+var children
+var closest_enemy
+var NMX
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -45,22 +47,15 @@ func _ready():
 	curr_pixel_speed = base_pixel_speed
 	curr_scroll_speed = base_scroll_speed
 	init_child_num = len(self.get_children())
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var New_enemy
-	var children
-	var closest_enemy
-	var NMX
 	
 	scrollDist += delta*base_pixel_speed
 	GlobalVariables.distance = scrollDist
   
 # Enemy spawn
-#	print(scrollDist,' ',fmod(scrollDist,spawn_position),' ',delta*curr_pixel_speed,' ',spawn_position-delta*curr_pixel_speed)
 	if (spawn_position-delta*curr_pixel_speed<fmod(scrollDist,spawn_position)) || (fmod(scrollDist,spawn_position)<delta*curr_pixel_speed):
-#		print('should spawn',' ',fmod(scrollDist,spawn_position))
 		New_enemy = Enemy_scn.instance()
 		New_enemy.set_global_position(Vector2(spawn_position,spawn_y))
 		New_enemy.pixel_speed=curr_pixel_speed
@@ -74,28 +69,6 @@ func _process(delta):
 		if (NMX<stopScroll+delta*curr_pixel_speed) && (NMX>stopScroll-delta*curr_pixel_speed):
 			woah_there(children,Bckgnd)
 
-#func slow_down(closest_enemy,startX,endX,slowTime):
-#	var tween = get_node("Tween")
-#	curr_pixel_speed = 0.0
-#	tween.interpolate_property(closest_enemy, "position:x",
-#		   startX,endX,slowTime,
-#		   Tween.TRANS_CUBIC, Tween.EASE_IN)
-#	tween.interpolate_property(Bckgnd, "material:shader_param/scroll_speed",
-#		   base_pixel_speed,0.0,slowTime,
-#		   Tween.TRANS_CUBIC, Tween.EASE_IN)
-#	tween.start()
-#	isSlowing = true
-#	pass
-#
-#func speed_up():
-#	isSlowing = false
-#	var tween = get_node("Tween")
-#	curr_pixel_speed = base_pixel_speed
-##	tween.interpolate_property(closest_enemy, "position:x",
-##	       startX,endX,slowTime,
-##		   Tween.TRANS_CUBIC, Tween.EASE_IN)
-##	tween.start()
-
 func woah_there(children,bckgnd):
 	bckgnd.set_scroll_speed(0.0)
 	for i in range(init_child_num,len(children)):
@@ -105,19 +78,19 @@ func woah_there(children,bckgnd):
 	
   
 func _on_PlayerControls_power1():
-	if power_slots[0]==0:
+	if power_slots[0] == powers.no_power:
 		power1_toggle.change_to_filled_texture()
-		power_slots[0]=1
+		power_slots[0]= powers.lightning
 	else:
 		power1_toggle.change_to_empty_texture()
-		power_slots[0] = 0
+		power_slots[0] = powers.no_power
 	
 	
 func _on_PlayerControls_power2():
-	if power_slots[1]==0:
+	if power_slots[1] == powers.no_power:
 		power2_toggle.change_to_filled_texture()
-		power_slots[1]=1
+		power_slots[1]= powers.lightning
 	else:
 		power2_toggle.change_to_empty_texture()
-		power_slots[1] = 0
+		power_slots[1] = powers.no_power
 
