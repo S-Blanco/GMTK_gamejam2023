781b8414d53aeb236f1b34141b184f2e7bada6f1
arjun21ch <arjun21596@gmail.com>
2023-07-09 12:02:38 +0200

    link to endgame scene readded

--- Game/Src/World/world.gd
+++ Game/Src/World/world.gd
@@ -5,13 +5,28 @@ onready var gloryUI = $CanvasLayer/GloryUI
 onready var dist = $CanvasLayer/GloryUI/distance
 onready var healthUI =$CanvasLayer/HealthUI
 onready var red = $CanvasLayer/HealthUI/Red
+onready var lost_legend = $EndgameScreen/CanvasLayer/LostLabel_Legend
+onready var lost_rubbish = $EndgameScreen/CanvasLayer/LostLabel_Rubbish
+onready var label = $EndgameScreen/CanvasLayer/Label
 
 func _process(_delta):
-	if GlobalVariables.glory > 0.03 and GlobalVariables.glory <= gloryUI.max_glory:
+	
+	
+	if GlobalVariables.glory > 0.03:
 		gloryUI.set_glory(GlobalVariables.glory)	
+		
+	if GlobalVariables.glory >= gloryUI.max_glory:
+		lost_legend.visible = true
+		label.visible = true
+	
+	if GlobalVariables.glory <= 0:
+		lost_rubbish.visible = true
+		label.visible = true
+		
 		
 	if GlobalVariables.distance > 0:
 		dist.text = "distance =" +str(GlobalVariables.distance)
+		label.text = "But you managed to run" +str(GlobalVariables.distance) + "kms"
 		
 	if GlobalVariables.damage > 0:
 		healthUI.set_damage(GlobalVariables.damage)
