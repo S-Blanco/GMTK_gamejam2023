93da9091b4d872abc4b351bc54f34d2134cec9ed
Yuglut <martin.farnir@uliege.be>
2023-07-09 09:59:50 +0100

    Player spawner + death added but many bugs

--- Game/Src/Characters/Character.gd
+++ Game/Src/Characters/Character.gd
@@ -4,6 +4,8 @@ onready var anim = $AnimationPlayer
 onready var animTree = $AnimationTree
 onready var animState = animTree.get("parameters/playback")
 
+signal died
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,8 +15,12 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func die():
+	death_animation()
+	emit_signal("died")
+	queue_free()
+
 func death_animation():
 	anim.get_animation("Disappear")
 	anim.play()
-	queue_free()
 	pass
